5448ba1bae558bc2a37bf79f57a4830bea28ca48
askid <askid.mobile@gmail.com>
2026-05-05 01:37:44 +0300

    Revert "fix(models): rectangular causal mask for prefix KV caching (#3437)"
    This reverts commit 058f601ff24a0f44dcd9dbcc8312d421e46f91f0.

--- candle-transformers/src/models/llama2_c.rs
+++ candle-transformers/src/models/llama2_c.rs
@@ -79,7 +79,7 @@ impl Config {
 
 #[derive(Debug, Clone)]
 pub struct Cache {
-    masks: HashMap<(usize, usize), Tensor>,
+    masks: HashMap<usize, Tensor>,
     pub use_kv_cache: bool,
     pub kvs: Vec<Option<(Tensor, Tensor)>>,
     pub cos: Tensor,
@@ -120,13 +120,15 @@ impl Cache {
         })
     }
 
-    pub fn mask(&mut self, seq_len: usize, index_pos: usize) -> Result<Tensor> {
-        let kv_len = index_pos + seq_len;
-        if let Some(mask) = self.masks.get(&(seq_len, kv_len)) {
+    pub fn mask(&mut self, t: usize) -> Result<Tensor> {
+        if let Some(mask) = self.masks.get(&t) {
             Ok(mask.clone())
         } else {
-            let mask = crate::utils::build_causal_mask(seq_len, index_pos, &self.device)?;
-            self.masks.insert((seq_len, kv_len), mask.clone());
+            let mask: Vec<_> = (0..t)
+                .flat_map(|i| (0..t).map(move |j| u8::from(j > i)))
+                .collect();
+            let mask = Tensor::from_slice(&mask, (t, t), &self.device)?;
+            self.masks.insert(t, mask.clone());
             Ok(mask)
         }
     }
@@ -203,7 +205,7 @@ impl CausalSelfAttention {
         let att = if seq_len <= 1 {
             att
         } else {
-            let mask = cache.mask(seq_len, index_pos)?.broadcast_as(att.shape())?;
+            let mask = cache.mask(seq_len)?.broadcast_as(att.shape())?;
             masked_fill(&att, &mask, f32::NEG_INFINITY)?
         };
         let att = candle_nn::ops::softmax(&att, D::Minus1)?;
