4e4b53bbaaf1e76d41feed393f52a17493433810
askid <askid.mobile@gmail.com>
2026-08-16 18:01:28 +0300

    chore: add run_candle_logits.bat helper

---
+++ run_candle_logits.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64
+set "PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.4\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.4\libnvvp;%PATH%"
+set "LIB=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.4\lib\x64;%LIB%"
+cd /d D:\Projects\yttri-build\candle-fork-qwen35-batch
+set QWEN36_LOGITS_FULL_STEPS=16,45,50,92,111
+cargo run --release --features real-model,cuda --bin qwen35moe_logits -- %*
