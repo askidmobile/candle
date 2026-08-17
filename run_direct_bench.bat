2041db76917e99bcb6606e3d59046134d609bf30
askid <askid.mobile@gmail.com>
2026-08-17 09:45:44 +0300

    chore: enable QWEN36_TRACE=1 in run_direct_bench.bat

--- run_direct_bench.bat
+++ run_direct_bench.bat
@@ -2,5 +2,6 @@
 setlocal
 set "PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.4\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.4\libnvvp;%PATH%"
 set "QWEN36_CUDA_GRAPHS=1"
+set "QWEN36_TRACE=1"
 cd /d D:\Projects\yttri-build\candle-fork-qwen35-batch
 target\release\bench_candle_direct.exe %*
